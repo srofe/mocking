5d3be2b24af4a300b8896226ca3fee375a22b874
Simon Rofe <simon.rofe@ieee.org>
2022-01-19 08:50:51 +1000

    Refactor to remove redundant spy
    The new test counts the number of times sleep and write are called, so
    the original spy is no longer required, and the insertion in the first
    test that used it can be removed.

--- countdown_test.go
+++ countdown_test.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
 	t.Run("The countdown function shall output a countdown, and call the sleep method.", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{}
-		Countdown(buffer, spySleeper)
+		Countdown(buffer, &CountdownOperationSpy{})
 		got := buffer.String()
 		want := `3
 2
@@ -18,16 +18,39 @@ Go!`
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
-		if spySleeper.Calls != 4 {
-			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	})
+
+	t.Run("The Countdown function shall call sleep and write alternatively.", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
 		}
 	})
 }
 
-type SpySleeper struct {
-	Calls int
+type CountdownOperationSpy struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
+
+func (s *CountdownOperationSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
