7576937812c7c93f75492bb1551bb106c84c8e51
Simon Rofe <simon.rofe@ieee.org>
2022-01-19 08:35:14 +1000

    Use a test spy to replace the sleep interface
    This allows us to track calls to sleep without having to wait for one
    second between each countdown. That way, the tests run faster but the
    implementation remains the same.

--- countdown.go
+++ countdown.go
@@ -10,15 +10,26 @@ import (
 const countdownStart = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- countdown_test.go
+++ countdown_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
-	Countdown(buffer)
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 	got := buffer.String()
 	want := `3
 2
@@ -16,4 +17,15 @@ Go!`
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+	}
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
 }
