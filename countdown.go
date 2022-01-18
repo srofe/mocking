83aed082fd7da1ab650d22946b4ddb9806fad399
Simon Rofe <simon.rofe@ieee.org>
2022-01-18 15:29:09 +1000

    Refactor to extract constants

--- countdown.go
+++ countdown.go
@@ -6,11 +6,14 @@ import (
 	"os"
 )
 
+const countdownStart = 3
+const finalWord = "Go!"
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
